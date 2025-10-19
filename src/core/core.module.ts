91d9b23f45b932198dfced5080fa497f72e8ac3f
RuzilKutliakhmetov <115926681+RuzilKutliakhmetov@users.noreply.github.com>
2025-10-19 16:10:29 +0500

    Настройка CookieParser, ExpressSession

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -7,6 +7,7 @@ import { IS_DEV_ENV } from '../shared/utils/is-dev.util'
 
 import { getGraphQLConfig } from './config/graphql.config'
 import { PrismaModule } from './prisma/prisma.module'
+import { RedisModule } from './redis/redis.module';
 
 @Module({
 	imports: [
@@ -20,7 +21,8 @@ import { PrismaModule } from './prisma/prisma.module'
 			inject: [ConfigService],
 			useFactory: getGraphQLConfig
 		}),
-		PrismaModule
+		PrismaModule,
+		RedisModule
 	]
 })
 export class CoreModule {}
